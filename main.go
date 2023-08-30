3d5dabcba51f75a5f1e5be412a6fa72e023b0117
daisyelem <daisyelem714@gmail.com>
2023-08-30 19:34:45 +0100

    data on webpage

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/daisyelem/RecipeKeeper/data"
 	"github.com/daisyelem/RecipeKeeper/handlers"
 )
 
 func main() {
+	data.FetchAllRecipes()
+	//fmt.Println(data.AllRecipes)
 	http.HandleFunc("/", handlers.HomePage)
 	http.ListenAndServe(":8000", nil)
 }
